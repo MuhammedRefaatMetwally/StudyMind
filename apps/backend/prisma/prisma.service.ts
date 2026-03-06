20e609adba14849b883ffed89fc690096c3f25e0
Muhammed <muhammedrefaat2020@gmail.com>
2026-03-06 04:36:09 +0200

    feat(auth): implement JWT authentication with register and login

--- apps/backend/prisma/prisma.service.ts
+++ apps/backend/prisma/prisma.service.ts
@@ -1,19 +1,9 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from './generated/prisma/client';
+import { Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  constructor() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL });
-    super({ adapter });
-  }
-
+export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-  }
-
-  async onModuleDestroy() {
-    await this.$disconnect();
   }
 }
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = NestFactory.create(AppModule);
+
+  (await app).useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips unknown fields automatically
+      forbidNonWhitelisted: true, // throws error if unknown fields sent
+      transform: true, // auto-transforms payloads to DTO instances
+    }),
+  );
+
+  (await app).enableCors();
+  (await app).setGlobalPrefix('api');
+
+  (await app).listen(3001);
 }
 bootstrap();
